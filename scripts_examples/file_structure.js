d2bfb7b6a4be817092f10ec36194ea1b9db15349
Liudmyla Deinychenko <liudmyla.deinychenko@radity.com>
2024-07-24 11:06:21 +0300

    add imports and file structure examples

---
+++ scripts_examples/file_structure.js
@@ -0,0 +1,40 @@
+/* For CommonJS keep file structure organized the following way: */
+// imports
+const { uniq } = require("lodash");
+const { auxKey } = require("../../constants.js");
+const { getTableValues } = require("../../utils.js");
+
+// constants (if not imported)
+const myFirstConstant = "my constant";
+
+// core logic
+const logOne = (title, message) => {
+  console.log(`${title}:${JSON.stringify(message)}`);
+};
+
+// main exports
+module.exports = {
+  log,
+};
+
+/* For ES Modules keep file structure organized the following way: */
+// imports
+import { uniq } from "lodash";
+import { auxKey } from "../../constants.js";
+import { getTableValues } from "../../utils.js";
+
+// constants (if not imported)
+const mySecondConstant = "my constant";
+
+// main exports
+export const waitingPeriodFactorCalculate = (
+  locFilingSetId,
+  polBiWaitingPeriod
+) => {
+  return getTableValues(CF13, [locFilingSetId, polBiWaitingPeriod])?.Factor;
+};
+
+// core logic
+const logTwo = (title, message) => {
+  console.log(`${title}:${JSON.stringify(message)}`);
+};
---
+++ scripts_examples/imports.js
@@ -0,0 +1,38 @@
+/* Import from the same file */
+// bad
+const { getAddress } = require("../lib/autofill/lib/utils.js");
+const { getFieldValue } = require("../lib/autofill/lib/utils.js");
+
+// good
+const { getAddress, getFieldValue } = require("../lib/autofill/lib/utils.js");
+
+/* Use destructuring to improve code readability
+Use camelCase for new module import names */
+// bad
+require("../lib/autofill/lib/utils.js");
+
+// bad
+const { GetAddress } = require("../lib/autofill/lib/utils.js");
+const { get_address } = require("../lib/autofill/lib/utils.js");
+const { GET_ADDRESS } = require("../lib/autofill/lib/utils.js");
+
+// good
+const { getAddress, getFieldValue } = require("../lib/autofill/lib/utils.js");
+
+/* Group and organize imports in following way:
+- external dependencies,
+- internal dependencies,
+- local imports */
+// bad
+const { getFieldValue } = require("../../utils.js");
+const polTaxFields = require("./polTaxFields.js");
+const { auxKey } = require("../../constants.js");
+const uniq = require("lodash");
+
+// good
+const uniq = require("lodash");
+
+const { getFieldValue } = require("../../utils.js");
+const { auxKey } = require("../../constants.js");
+
+const polTaxFields = require("./polTaxFields.js");
